f901a50de10dc28eb334de4e55d3ede437c1dc9f
Luara <albuquerquebluara@gmail.com>
2022-04-28 23:47:39 -0300

    exercicio5

---
+++ Exercicio5/Program.cs
@@ -0,0 +1,12 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*Escreva um programa em C# e no Visual Studio para calcular o consumo médio de um automóvel
+(medido em Km/l), sendo que são conhecidos a distância total percorrida e o volume de combustível
+consumo para percorrê-la (medido em l)
+*
+*/
+Console.WriteLine("Exercicio 5: Escreva um programa em C# e no Visual Studio para calcular o consumo\n" +
+    " médio de um automóvel (medido em Km / l), sendo que são conhecidos a distância total percorrida\n" +
+    " e o volume de combustível consumo para percorrê - la(medido em l)");
+
+Console.WriteLine("Informe o consumo medo por litro")
