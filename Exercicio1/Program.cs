c983c19008138e1eb9eb041dc9aee278df227bd6
Luara <albuquerquebluara@gmail.com>
2022-04-28 22:38:39 -0300

    Exercicio1

---
+++ Exercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+ * 1) Escreva um programa em C# e no Visual Studio para calcular a média aritmética entre números quaisquer.
+*
+*/
+Console.WriteLine("Exercício 1: Escreva um programa em C# e no Visual Studio para calcular a média\n" +
+                  "aritmética entre números quaisquer.");
+
+
+
+float numero1;
+float numero2;
+float media_aritmedica;
+
+Console.Write("Digite um número qualquer:  ");
+numero1 = float.Parse(Console.ReadLine());
+
+Console.Write("Digite um segundo número qualquer:  ");
+numero2 = float.Parse(Console.ReadLine());
+
+media_aritmedica = numero1 / numero2;
+
+Console.WriteLine("A média arittmedica dos valores é de: " + media_aritmedica);
