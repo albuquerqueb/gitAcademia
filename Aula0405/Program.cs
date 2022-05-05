3030dda1de0e07df2c9f1d42f2f78a77037773cb
Luara <albuquerquebluara@gmail.com>
2022-05-04 21:23:05 -0300

    aula0405

---
+++ Aula0405/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+double num1, num2, resultado;
+int op;
+Console.WriteLine("Digite dois numeros");
+num1 = Double.Parse(Console.ReadLine());
+num2 = Double.Parse(Console.ReadLine());
+Console.WriteLine("\nEscolha a operação: \n1.Soma\n2.Subtração\n3.Multiplicação\n4.Divisão\n5.Potencia\nEscola:");
+op = int.Parse(Console.ReadLine());
+if (op == 1)
+{
+    resultado = num1 + num2;
+    Console.WriteLine("Soma = " + resultado);
+
+}
+else if (op == 2)
+{
+    resultado = num1 + num2;
+    Console.WriteLine("Subtração = " + resultado);
+}
+else if (op == 3)
+{
+    resultado = num1 + num2;
+    Console.WriteLine("Multiplicação = " + resultado);
+}
+else if (op == 4)
+{
+    resultado = num1 + num2;
+    Console.WriteLine("Divisão = " + resultado);
+}
+else if (op == 5)
+{
+    resultado = num1 + num2;
+    Console.WriteLine("Subtração = " + resultado);
+}
---
+++ Exercicio12/Program.cs
@@ -0,0 +1,47 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/* Um motorista de taxi deseja calcular o rendimento de seu carro na praça. Sabendo-se que o preço do combustível/n
+ * é de R$6,90, escreva um algoritmo para ler a marcação do odômetro (marcador de quilometragem) no início do dia,/n
+ * a marcação no final do dia, o número de litros de combustível gasto e o valor total (R$) recebido/n
+ * dos passageiros. Calcule e escreva a média do consumo em Km/l e o lucro líquido do dia.*/
+
+float odometroinicio;
+float odometrofinal;
+float litrosgasto;
+float valorrecebeu;
+float media;
+float lucro;
+float total;
+float lucro;
+
+Console.WriteLine("Informe seu marcador do odometro do inicio do dia");
+odometroinicio = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe seu marcador do odometro do final do dia");
+odometrofinal = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe os litros que foi utilizado no final do dia");
+litrosgasto = float.Parse(Console.ReadLine());
+
+Console.WriteLine("Informe valor que recebeu no final do dia");
+valorrecebeu = float.Parse(Console.ReadLine());
+
+total = odometroinicio + odometrofinal;
+
+media = total / litrosgasto;
+
+lucro = float.Parse(valorrecebeu - litrosgasto * 6,90()); 
+
+Console.WriteLine("O total de Km/l consumido por dia é: " + total);
+
+Console.WriteLine("A media consumida por dia é: " + media);
+
+Console.WriteLine("O lucro no fim do dia foi: " + lucro);
+
+
+
+
+
+
+
+
